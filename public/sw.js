5b261dbf8910b00a553f9dde45cf0e0a1e4d4b0e
design-indra <ghostersoloist@gmail.com>
2026-03-14 10:13:07 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,41 @@
+const CACHE_NAME = 'gaple-v1'
+const STATIC_ASSETS = [
+  '/',
+  '/lobby',
+  '/_next/static/',
+]
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/'])
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  // Network first untuk API, cache first untuk assets
+  if (e.request.url.includes('/api/') || e.request.url.includes('supabase')) {
+    return // biarkan network handle
+  }
+
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
