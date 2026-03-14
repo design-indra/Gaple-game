3b7bbcdd0358ae489e4adc22a8c7b66d1bc4e6c4
design-indra <ghostersoloist@gmail.com>
2026-03-14 10:11:56 +0700

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {})
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <meta name="application-name" content="Gaple Online" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Gaple" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#1a5c2e" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="icon" href="/icons/icon-192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
